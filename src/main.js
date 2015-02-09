dec37cec95762e68831e207b87a8cb340a284d7b
bivdub <bivdub@gmail.com>
2015-02-08 23:27:06 -0800

    fixes capitalization error

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 define(function(require, exports, module) {
   var View    = require('famous/core/View');
   var Engine  = require('famous/core/Engine');
-  var AppView = require('views/Appview')
+  var AppView = require('views/AppView')
 
   var mainContext = Engine.createContext();
   var appView = new AppView();
